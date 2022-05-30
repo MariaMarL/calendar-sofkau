634db8289beca4d9b2abd6a831f72591fec72960
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-05-29 22:58:03 -0500

    second test working properly

--- src/test/java/co/com/sofka/calendar/services/SchedulerServiceTest.java
+++ src/test/java/co/com/sofka/calendar/services/SchedulerServiceTest.java
@@ -87,11 +87,11 @@ class SchedulerServiceTest {
         Mockito.when(repository.findById(programId)).thenReturn(Mono.empty());
 
         //TODO: hacer de otro modo
-        var exception = Assertions.assertThrows(RuntimeException.class, () -> {
-            schedulerService.generateCalendar(programId, startDate);//TODO: hacer una subscripción de el servicio reactivo
+        Flux<ProgramDate> response = schedulerService.generateCalendar(programId, startDate);
+        StepVerifier.create(response).expectErrorMessage("Non existing program").verify();
 
-        });
-        Assertions.assertEquals("El programa academnico no existe", exception.getMessage());//TODO: hacer de otro modo
+        StepVerifier.create(response).expectError(RuntimeException.class);
+
         Mockito.verify(repository).findById(programId);
 
     }
